6dfd3e9a3ffa9df35d0fe80fbd3244c5f34eda32
sukruth-gms <138142092+sukruth-gms@users.noreply.github.com>
2024-03-11 13:38:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import random
 
 app = Flask(__name__)
 
-# list of cat images
+# list of images
 images = [
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
